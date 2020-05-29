fb63f3258017b8aec67859a7c73cd960134faa4d
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-29 21:12:32 +0800

    add item

--- components/Todo.js
+++ components/Todo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 
 class Todo extends React.Component {
@@ -11,30 +11,46 @@ class Todo extends React.Component {
         }
     }
 
-  
+
     handleTextChange = (text) => {
         this.setState({
             input: text
         })
     }
     handleAddPress = () => {
-            this.props.addItem(this.state.input)
-         
+        this.props.addItem(this.state.input)
+
 
     }
     render() {
         return (
             <View style={styles.container}>
                 <View style={{ flexDirection: 'row' }}>
-                    <TextInput placeholder="Add item name" onChangeText={(text) => 
+                    <TextInput placeholder="Add item name" onChangeText={(text) =>
                         this.handleTextChange(text)}></TextInput>
                     <Button title="Add" onPress={() => this.handleAddPress()}></Button>
                 </View>
                 <FlatList
                     data={this.props.todos}
                     renderItem={({ item }) =>
-                        <View>
-                            <Text>{item.name}</Text>
+                        <View style={styles.item}>
+                            {/* Ternary operator  - Normal JS */}
+                            {
+                                item.completed  ? <Text>Completed </Text>: <Text>Not Completed</Text>
+                            }
+                            <Text style={{ flex: 3 ,fontSize:20}}>{item.name}</Text>
+                            <TouchableOpacity style={{ flex: 1 ,backgroundColor:'red', padding:20}} 
+                            onPress={()=> this.props.deleteTodo(item.id)}>
+                                <View>
+                                    <Text>Delete</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{ flex: 1,backgroundColor:'orange', padding:20}} 
+                            onPress={()=> this.props.markAsDone(item.id)}>
+                                <View>
+                                    <Text>Done</Text>
+                                </View>
+                            </TouchableOpacity>
                         </View>
                     }
                     keyExtractor={item => item.id}
@@ -44,15 +60,17 @@ class Todo extends React.Component {
     }
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
     return {
-        todos:state.todos
+        todos: state.todos
     }
 }
-const mapDispatchToProps= (dispatch) =>{
+const mapDispatchToProps = (dispatch) => {
     return {
         //Put text to pass text to props
-        addItem: (text) => dispatch({type:'ADD_TODO',text})
+        addItem: (text) => dispatch({ type: 'ADD_TODO', text }),
+        deleteTodo: (id) => dispatch({type:'DELETE_TODO',id}),
+        markAsDone: (id) => dispatch({type:'MARK_AS_DONE',id})
     }
 }
 const styles = StyleSheet.create({
@@ -62,6 +80,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: 40
     },
+    item: { flexDirection: 'row', width: 400, 
+    backgroundColor: 'lightyellow', 
+    paddingHorizontal:20, 
+    marginVertical:10,
+    alignItems:'center'
+    
+ }
 });
 
-export default connect(mapStateToProps,mapDispatchToProps)(Todo)
+export default connect(mapStateToProps, mapDispatchToProps)(Todo)
--- reducers/reducer.js
+++ reducers/reducer.js
@@ -1,19 +1,46 @@
 const initialState = {
     todos: []
 }
+let number = 0;
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_TODO':
-            
-            const newItem = { id: 1, name: action.text };
-          
+
+            const newItem = { id: number++, name: action.text, completed: false };
+
+            //; get the current array, but add newItem at the end - es6 fcc or redux fcc (you will do it during next week break)
+            // In redux use this instead of push
             const newArray = [...state.todos, newItem];
-   
-            return {...state,todos:newArray}
+            // get the current object, but change the todos value to new Array 
+            // In redux, use this instead of . [""] = state["todos"] = newArray
+            return { ...state, todos: newArray }
+        case 'DELETE_TODO':
+
+            const deletedItem = [...state.todos.slice(0, action.id), ...state.todos.slice(action.id + 1)]
+            return { ...state, todos: deletedItem };
+
+        case 'MARK_AS_DONE':
+
+        // Refer to array map inside functional programming, es6  [... ], {... }
+            const arrayItem = [...state.todos.map(val => {
+                if (val.id == action.id) {
+                    return { ...val, completed: true }
+                }
+                return val
+            })]
+            return {...state, todos: arrayItem};
         default:
             return state;
     }
 }
 
 export default reducer;
+
+// To add 2 buttons in every row
+// 1 button will have delete function 
+// Another button will have mark as done
+
+// Make sure that all the button works
+// For delete it will remove from the list
+//For mark as done you can  show done or tick if it is done
